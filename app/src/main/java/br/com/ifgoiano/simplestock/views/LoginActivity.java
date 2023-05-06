a70a51b327778a24e3bdd53723361d0676082e4b
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-05-06 10:03:23 -0300

    iniciado construção da tela de login

--- app/src/main/java/br/com/ifgoiano/simplestock/views/LoginActivity.java
+++ app/src/main/java/br/com/ifgoiano/simplestock/views/LoginActivity.java
@@ -1,14 +1,16 @@
-package br.com.ifgoiano.simplestock;
+package br.com.ifgoiano.simplestock.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.ifgoiano.simplestock.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
